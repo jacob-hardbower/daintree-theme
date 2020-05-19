f07044d006453a9a31ba2ec44a9a828034340cc9
jacob-hardbower <54637739+jacob-hardbower@users.noreply.github.com>
2020-05-19 14:04:10 -0400

    Introduce some boilerplate code

--- static/site.js
+++ static/site.js
@@ -1,5 +1,58 @@
 jQuery( document ).ready( function( $ ) {
 
-  console.log("hey");
+  $("body").on("click", "#menu-button", function() {
+    // Swap text in the menu button to close and back
+    $("#menu-button").toggleText('Menu', 'Close');
+    // Add open class to header
+    $(".site-header").toggleClass("nav-open");
+
+    // Disable scrolling when menu is open
+    if($(".site-header").hasClass("nav-open")) {
+      lockScroll();
+    } else {
+      unlockScroll();
+    }
+  });
+
+  $("body").on("click", ".nav-main a", function() {
+    // Swap text in the menu button to close and back
+    $("#menu-button").toggleText('Menu', 'Close');
+    // Add open class to header
+    $(".site-header").removeClass("nav-open");
+
+    // Disable scrolling when menu is open
+    if($(".site-header").hasClass("nav-open")) {
+      lockScroll();
+    } else {
+      unlockScroll();
+    }
+  });
+
+  $(document).keyup(function(e) {
+    if(e.key === "Escape") {
+      $(".site-header").removeClass("nav-open");
+      unlockScroll();
+    }
+  });
+
+  // Used to toggle text
+  // https://stackoverflow.com/a/39525988
+  $.fn.extend({
+    toggleText: function(a, b){
+        return this.text(this.text() == b ? a : b);
+    }
+  });
+
+  function lockScroll() {
+    $('html, body').css({
+        overflow: 'hidden'
+    });
+  }
+
+  function unlockScroll() {
+    $('html, body').css({
+        overflow: 'visible'
+    });
+  }
 
 });
