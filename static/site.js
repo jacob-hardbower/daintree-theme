b77913072e00e1fa668af9850f66332cb1e39ed6
jacob-hardbower <54637739+jacob-hardbower@users.noreply.github.com>
2020-05-22 08:52:06 -0400

    Implement boilerplate scss files

--- static/site.js
+++ static/site.js
@@ -1,24 +1,15 @@
 jQuery( document ).ready( function( $ ) {
 
-  $("body").on("click", "#menu-button", function() {
+  // UNUSED - Stickykit - stick header to top of viewport
+  // $(".site-header").stick_in_parent();
+
+  checkHeight();
+
+  $("#menu-button").on("click", function() {
     // Swap text in the menu button to close and back
     $("#menu-button").toggleText('Menu', 'Close');
     // Add open class to header
     $(".site-header").toggleClass("nav-open");
-
-    // Disable scrolling when menu is open
-    if($(".site-header").hasClass("nav-open")) {
-      lockScroll();
-    } else {
-      unlockScroll();
-    }
-  });
-
-  $("body").on("click", ".nav-main a", function() {
-    // Swap text in the menu button to close and back
-    $("#menu-button").toggleText('Menu', 'Close');
-    // Add open class to header
-    $(".site-header").removeClass("nav-open");
 
     // Disable scrolling when menu is open
     if($(".site-header").hasClass("nav-open")) {
@@ -56,3 +47,46 @@ jQuery( document ).ready( function( $ ) {
   }
 
 });
+
+/**
+ * Debounced function for applying "stuck" flag to header.
+ * @return {function}
+ */
+var checkHeight = debounce(function() {
+  if(window.scrollY==0) {
+    var element = document.getElementById("site-header");
+    if(element.classList.contains("stuck")) {
+      element.classList.remove("stuck");
+    }
+  } else {
+    var element = document.getElementById("site-header");
+    if(!element.classList.contains("stuck")) {
+      element.classList.add("stuck");
+    }
+  }
+}, 0);
+
+
+/**
+ * Prevents code from running too often.
+ * @param  {function} func      The function containing your code to be dounced.
+ * @param  {int} wait           Milliseconds to wait between running function code.
+ * @param  {boolean} immediate  If true, triggers function on leading edge instead of trailing.
+ * @return {function}
+ */
+function debounce(func, wait, immediate) {
+  var timeout;
+  return function executedFunction() {
+    var context = this;
+    var args = arguments;
+    var later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+
+};
